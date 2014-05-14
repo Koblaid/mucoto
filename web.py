67d2af79026bcbaf7043969a7a79965da0341046
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2014-05-14 11:20:42 +0200

    Added more timers

--- web.py
+++ web.py
@@ -8,11 +8,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    artists = main.parse_directory(dir_path)
+    dir_path = '/run/media/ben/6187082c-cf9e-4f8d-829c-6d71ba19f11f/d2/d/music/'
+
     start = time.time()
-    duration = time.time() - start
+    artists = main.parse_directory(dir_path)
+    dir_duration = time.time() - start
+
+    start = time.time()
+    main.read_tags(artists)
+    tag_duration = time.time() - start
+
+    start = time.time()
     stats = main.stats(artists)
-    return render_template('index.html', artists=artists, stats=stats, duration=duration)
+    stats_duration = time.time() - start
+
+    return render_template('index.html', artists=artists, stats=stats, dir_duration=dir_duration, tag_duration=tag_duration, stats_duration=stats_duration)
 
 if __name__ == '__main__':
     app.run(debug=True)
